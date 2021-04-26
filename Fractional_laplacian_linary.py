0ca39b84efa6768cf057bf7a2e5665ba3dec1a34
Malinon <jakubbmalinowski@gmail.com>
2021-04-26 19:56:25 +0200

    Add single precision mode
    After this commit precision mode can be controlled via double_precision
    parameter in F. L. Calculators.

--- Fractional_laplacian_linary.py
+++ Fractional_laplacian_linary.py
@@ -4,14 +4,20 @@ import numpy as np
 
 class FractionalLaplacianAproximationLinary(FractionalLaplacianAproximationBase):
 
-    def __init__(self, alpha, h, num_of_steps, func, sum_method, dim = 1):
-        super().__init__(alpha, h, num_of_steps, func, sum_method, dim)
+    def __init__(self, alpha, h, num_of_steps, func, sum_method, dim = 1,
+    double_precision = True):
+        super().__init__(alpha, h, num_of_steps, func, sum_method, dim, double_precision)
 
 
         f_func_gen = FFunction(alpha, h)
         f_func_gen.set_C_alpha_1(self.C_ALPHA_1)
 
-        self.F_FUNCTION = f_func_gen.gen_F_fun()
+        f_fun = f_func_gen.gen_F_fun()
+        if double_precision:
+            self.F_FUNCTION = lambda t: f_fun(np.float32(t))
+        else:
+            self.F_FUNCTION = f_fun
+
         self.F_DERIVATIVE_AT_1 = f_func_gen.gen_F_derivative_at_1()
         self.GENERAL_MULTI = f_func_gen.gen_general_multiplication()
         self.sum_method = sum_method
@@ -23,11 +29,12 @@ class FractionalLaplacianAproximationLinary(FractionalLaplacianAproximationBase)
     def calculate_w_j_params(self):
         print("Linary")
         w_j_params = np.zeros(int(self.num_of_steps))
-        w_j_params[0] = (1.0 / (2.0 - self.ALPHA) - self.F_DERIVATIVE_AT_1 +
-        self.F_FUNCTION(2) + self.F_FUNCTION(1))
+        w_j_params[0] = (self.CONST_ONE / (self.CONST_TWO
+        - self.ALPHA) - self.F_DERIVATIVE_AT_1 +
+        self.F_FUNCTION(self.CONST_TWO) + self.F_FUNCTION(self.CONST_ONE))
 
         for j in range(2, int(self.num_of_steps) + 1):
-            w_j_params[j - 1] = (self.F_FUNCTION(j + 1) - 2 * self.F_FUNCTION(j)
-            + self.F_FUNCTION(j - 1))  
+            w_j_params[j - 1] = (self.F_FUNCTION(j + 1) - self.CONST_TWO
+            * self.F_FUNCTION(j) + self.F_FUNCTION(j - 1))  
 
         return w_j_params

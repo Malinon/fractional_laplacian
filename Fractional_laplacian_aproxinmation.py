Fix single precision errors
After this commit precision modes (single vs double) work correctly

Fractional_laplacian_aproxinmation.py
@@ -1,3 +1,4 @@
+from __future__ import print_function
 from utils_func_essential import calculate_c_alpha_1
 import numpy as np
 
@@ -11,19 +12,20 @@ class FractionalLaplacianAproximationBase(object):
             self.H = h
             self.ALPHA = alpha
             self.DIM = dim
+            self.components_of_sum = np.zeros(num_of_steps * 2)
             self.func_at = lambda index_of_point: (
-                self.FUNC(index_of_point * self.H))
+                func(index_of_point * self.H))
         else:
             self.CONST_ONE = np.float32(1)
             self.CONST_TWO = np.float32(2)
             self.H = np.float32(h)
             self.ALPHA = np.float32(alpha)
             self.DIM = np.float32(dim)
+            self.components_of_sum = np.zeros(num_of_steps * 2, dtype=np.float32)
             self.func_at = lambda index_of_point: (
-                self.FUNC(np.float32(index_of_point * self.H)))
+                func(np.float32(index_of_point * self.H)))
         
         self.num_of_steps = num_of_steps
-        self.FUNC = func
         self.C_ALPHA_1 = calculate_c_alpha_1(self.ALPHA, self.DIM, double_precision)
 
         self.sum_method = sum_method
@@ -38,9 +40,9 @@ class FractionalLaplacianAproximationBase(object):
 
 # Calculate singular part of integral
     def get_value_singular(self, index_of_point):
-        u_i = self.FUNC(self.H * index_of_point)
-        u_i_next = self.FUNC(self.H * (index_of_point + 1))
-        u_i_prev = self.FUNC(self.H * (index_of_point - 1))
+        u_i = self.func_at(self.H * index_of_point)
+        u_i_next = self.func_at(self.H * (index_of_point + 1))
+        u_i_prev = self.func_at(self.H * (index_of_point - 1))
         singu =-self.C_ALPHA_1 * (self.H ** (-self.ALPHA)) * (
             u_i_next - self.CONST_TWO * u_i + u_i_prev) / (self.CONST_TWO
             - self.ALPHA)
@@ -50,15 +52,15 @@ class FractionalLaplacianAproximationBase(object):
     def get_value_tail(self, index_of_point):
         if self.num_of_steps == 0:
             return 0
-        components_of_sum = np.zeros(int(self.num_of_steps) * 2)
+        
         u_i = self.func_at(index_of_point)
         index = 0
         index_val = 1
         for w in self.w_j_list:
-            components_of_sum[index] = w * (u_i - self.func_at(index_of_point - index_val))
+            self.components_of_sum[index] = w * (u_i - self.func_at(index_of_point - index_val))
             index = index + 1
-            components_of_sum[index] = w * (u_i - self.func_at(index_of_point + index_val))
+            self.components_of_sum[index] = w * (u_i - self.func_at(index_of_point + index_val))
             index = index + 1
             index_val = index_val + 1
 
-        return self.GENERAL_MULTI * (self.sum_method(components_of_sum))
+        return self.GENERAL_MULTI * (self.sum_method(self.components_of_sum))

Fractional_laplacian_linary.py
@@ -9,14 +9,14 @@ class FractionalLaplacianAproximationLinary(FractionalLaplacianAproximationBase)
         super().__init__(alpha, h, num_of_steps, func, sum_method, dim, double_precision)
 
 
-        f_func_gen = FFunction(alpha, h)
+        f_func_gen = FFunction(self.ALPHA, self.H)
         f_func_gen.set_C_alpha_1(self.C_ALPHA_1)
 
         f_fun = f_func_gen.gen_F_fun()
         if double_precision:
-            self.F_FUNCTION = lambda t: f_fun(np.float32(t))
-        else:
             self.F_FUNCTION = f_fun
+        else:
+            self.F_FUNCTION = lambda t: f_fun(np.float32(t))
 
         self.F_DERIVATIVE_AT_1 = f_func_gen.gen_F_derivative_at_1()
         self.GENERAL_MULTI = f_func_gen.gen_general_multiplication()
@@ -28,7 +28,7 @@ class FractionalLaplacianAproximationLinary(FractionalLaplacianAproximationBase)
 
     def calculate_w_j_params(self):
         print("Linary")
-        w_j_params = np.zeros(int(self.num_of_steps))
+        w_j_params = np.zeros(int(self.num_of_steps), dtype=np.float32)
         w_j_params[0] = (self.CONST_ONE / (self.CONST_TWO
         - self.ALPHA) - self.F_DERIVATIVE_AT_1 +
         self.F_FUNCTION(self.CONST_TWO) + self.F_FUNCTION(self.CONST_ONE))
@@ -36,5 +36,4 @@ class FractionalLaplacianAproximationLinary(FractionalLaplacianAproximationBase)
         for j in range(2, int(self.num_of_steps) + 1):
             w_j_params[j - 1] = (self.F_FUNCTION(j + 1) - self.CONST_TWO
             * self.F_FUNCTION(j) + self.F_FUNCTION(j - 1))  
-
         return w_j_params

utils_func_check.py
@@ -6,16 +6,17 @@ import scipy.special as ss
 from utils_func_essential import calculate_c_alpha_1
 
 
-def calculators_gen_linear(alpha, num_of_steps, hs, func, double_precision = True):
+def calculators_gen_linear(alpha, num_of_steps_list, hs, func, double_precision = True):
     print(hs)
-    return [FractionalLaplacianAproximationLinary(alpha = alpha, num_of_steps=num_of_steps,
-        func = func, sum_method =kahan_sum, h = h,
-        double_precision = double_precision) for h in hs ]
+    print(num_of_steps_list)
+    return [FractionalLaplacianAproximationLinary(alpha = alpha, num_of_steps=tup[1],
+        func = func, sum_method =kahan_sum, h = tup[0],
+        double_precision = double_precision) for tup in zip(hs,num_of_steps_list)  ]
 
-def calculators_gen_quad(alpha, num_of_steps, hs, func, double_precision = True):
-    return [FractionalLaplacianAproximationQuad(alpha = alpha, num_of_steps=num_of_steps,
-        func = func, sum_method =kahan_sum, h = h,
-        double_precision = double_precision) for h in hs ]
+def calculators_gen_quad(alpha, num_of_steps_list, hs, func, double_precision = True):
+    return [FractionalLaplacianAproximationQuad(alpha = alpha, num_of_steps=tup[2],
+        func = func, sum_method =kahan_sum, h = tup[1],
+        double_precision = double_precision) for tup in zip(hs,num_of_steps_list)]
 
 
 def calculate_part_II(alpha, func, L, x):

utils_func_essential.py
@@ -12,13 +12,13 @@ def calculate_c_alpha_1( alpha, dim = 1, double_precision = True):
         ONE = np.float32(1.0)
         TWO = np.float32(2.0)
         PI = np.float32(math.pi)
-    return (alpha * special.gamma( (alpha + dim) / TWO ) * (
-            TWO ** (alpha - ONE))) / ((PI ** (dim / TWO))
+    return (alpha * special.gamma( (alpha + dim) / TWO ) * np.power(
+            TWO, (alpha - ONE))) / (np.power(PI, (dim / TWO))
             * special.gamma((TWO - alpha) / TWO))
 
 def kahan_sum(components):
     sum_val = components[0]
-    correct_val = 0.0
+    correct_val = sum_val - sum_val # Because of type
     for i in range(1, len(components)):
         y = components[i] - correct_val
         t = sum_val + y
@@ -47,7 +47,7 @@ class FFunction:
         if self.ALPHA == self.CONST_ONE:
             return lambda t : -np.log(t)
 
-        return lambda t : (t ** (self.CONST_ONE - self.ALPHA)) / (
+        return lambda t : np.power(t, (self.CONST_ONE - self.ALPHA)) / (
                 (self.ALPHA - self.CONST_ONE) * self.ALPHA)
 
     def gen_F_derivative_at_1(self):
@@ -55,7 +55,7 @@ class FFunction:
 
     # All componenets are mupltiplied by C_1_self.ALPHA / h^alpha
     def gen_general_multiplication(self):
-        return (self.H ** (-self.ALPHA)) * self.C_1_ALPHA
+        return np.power(self.H, (-self.ALPHA)) * self.C_1_ALPHA
 
 class GFunction:
     # G will be called with only non-negative args, so abs was deleted
@@ -77,14 +77,14 @@ class GFunction:
         if self.ALPHA == self.CONST_ONE:
             return lambda t : t - t * np.log(t)
 
-        return lambda t : (t ** (self.CONST_TWO - self.ALPHA)) / (
+        return lambda t :np.power(t ,(self.CONST_TWO - self.ALPHA)) / (
                 (self.CONST_TWO - self.ALPHA) * (self.ALPHA - self.CONST_ONE) * self.ALPHA)
 
     def gen_G_fun_derivative(self):
         if self.ALPHA == self.CONST_ONE:
             return lambda t: -np.log(t)
 
-        return lambda t:  (t ** (self.CONST_ONE - self.ALPHA)) / (self.ALPHA *
+        return lambda t:  np.power(t, (self.CONST_ONE - self.ALPHA)) / (self.ALPHA *
         (self.ALPHA - self.CONST_ONE))
 
     def gen_G_second_derivative_at_1(self):
@@ -92,4 +92,4 @@ class GFunction:
 
     # All componenets are mupltiplied by C_1_self.ALPHA / h^alpha
     def gen_general_multiplication(self):
-        return (self.H ** (-self.ALPHA)) * self.C_1_ALPHA
+        return np.power(self.H, (-self.ALPHA)) * self.C_1_ALPHA